53f6799a22beccdf8c769587a893f626e4879d1e
Maxim Sokhatsky <maxim@synrc.com>
2013-04-27 11:40:24 +0300

    render code

--- hi.js
+++ hi.js
@@ -7,7 +7,7 @@ var keywords = ('fun begin if else receive end case where').split(' '),
 var syntax = [
   ['comment', /(\/\*(?:[^*\n]|\*+[^\/*])*\*+\/)/g],
   ['comment', /(\%%[^\n]*)/g],
-  ['record', /(\#[^{]*)/g],
+  ['record', /(\#[^{']*)/g],
   ['string' , /("(?:(?!")[^\\\n]|\\.)*"|'(?:(?!')[^\\\n]|\\.)*')/g],
   ['regexp' , /(\/.+\/[mgi]*)(?!\s*\w)/g],
   ['class'  , /\b([A-Z][a-zA-Z]+)\b/g],
