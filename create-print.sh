467f0e2958c1ff719cf99abb340b69426a515727
Maxim Sokhatsky <maxim@synrc.com>
2013-05-03 11:54:05 +0300

    n2o cookbook (TeX)

--- create-print.sh
+++ create-print.sh
@@ -2,13 +2,20 @@
 
 # Synrc TeX Templates
 
+PWD=`pwd`
 create_tex()
 {
-	latex $2/$1.tex -output-directory=$2
+    cd $2
+    latex $1.tex
+    cd $PWD
 }
 
-create_tex index research/io
-create_tex index .
-create_tex labs .
-create_tex github .
-
+#create_tex index research/io
+#create_tex index .
+#create_tex labs .
+#create_tex github .
+create_tex index framework/web
+create_tex elements framework/web
+create_tex actions framework/web
+create_tex api framework/web
+create_tex extending framework/web
