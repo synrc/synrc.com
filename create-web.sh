dfb2917c163a5ad9220a25e889daac282470cbc8
Maxim Sokhatsky <maxim@synrc.com>
2013-08-09 06:28:59 +0300

    synrc site changes

--- create-web.sh
+++ create-web.sh
@@ -21,6 +21,8 @@ create_page client/sync/windows/index
 create_page client/chat/haiku/index
 create_page beos/beos_collection
 create_page news/erlang2013kiev
+create_page news/kievfprog2013
+create_page news/hotcode2013
 create_page news/index
 create_page research/io/doc/overview
 create_page research/io/doc/object
