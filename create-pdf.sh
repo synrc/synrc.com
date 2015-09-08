9b7e44d7de77dfc9433eb3d7471c2d42be14adfc
Namdak Tonpa <maxim@synrc.com>
2015-09-08 13:59:38 +0300

    bpe

--- create-pdf.sh
+++ create-pdf.sh
@@ -13,5 +13,5 @@ create_pdf()
 #create_pdf ~/depot/spawnproc/upl
 #create_pdf ~/depot/spawnproc/forms
 #create_pdf ~/depot/spawnproc/bpe
-
-create_pdf ~/depot/synrc/n2o
+#create_pdf ~/depot/synrc/n2o
+create_pdf ~/depot/spawnproc/bpe
